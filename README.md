c57b1b0418b7819b52a3a1d5b6a2bae9905ed50a
javier <javier.morabaiz@gmail.com>
2025-04-14 23:06:19 -0300

    end of the functions, tested and project finished

--- README.md
+++ README.md
@@ -2,3 +2,4 @@ Practicing with JS, Postman and HTML making a tasks and reminders app
 
 utilizando jsonplaceholder 
 para el uso de get y push , para trare posteos, eliminar y editar
+4 metodos para hacer un CRUD
--- js/script.js
+++ js/script.js
@@ -3,31 +3,35 @@ let tasks = [] // initialize the posts as an empty array
 
 function getData(){ // TO DO
     fetch(urlBase)
-    .then(res => res.json())
-    .then(data =>{
-        tasks = data
-        renderTaskList()
-    })
-    .catch(error => console.error('Error when calling the API: ',error))
+        .then(res => res.json())
+        .then(data => {
+            tasks = data
+            renderTaskList()
+        })
+        .catch(error => console.error('Error when calling the API: ',error))
 }
+getData()
 
 // renderizado las tareas
 function renderTaskList(){
     const postList = document.getElementById('postList')
     postList.innerHTML = ''
-    tasks.forEach(task =>{
+
+    tasks.forEach(task => {
         const listTask = document.createElement('li')
         listTask.classList.add('postItem')
         listTask.innerHTML = `
+
         <strong>${task.title}</strong>
         <p>${task.body}</p>
         <button onclick="editTask(${task.id})">Edit</button>
         <button onclick="deleteTask(${task.id})">Delete</button>
+
         <div id="editForm-${task.id}" class="editForm" style="display:none">
             <label for="editTitle">Title: </label>
             <input type="text" id="editTitle-${task.id}" value="${task.title}" required>
-            <label for="editBody">To do:</label>
-            <textarea id="editBody-${task.id}" required></textarea>
+            <label for="editBody">To do: </label>
+            <textarea id="editBody-${task.id}" required>${task.body}</textarea>
             <button onclick="updateTask(${task.id})">Update</button>
         </div>
         `
@@ -36,12 +40,12 @@ function renderTaskList(){
 }
 
 function postData(){
-    const postTitleInput = document.getElementById('postTitle').value  // selected info from html
-    const postBodyInput = document.getElementById('postBody').value    // selected info from html
+    const postTitleInput = document.getElementById('postTitle') // selected info from html
+    const postBodyInput = document.getElementById('postBody')   // selected info from html
     const postTitle = postTitleInput.value
     const postBody = postBodyInput.value
 
-    if (postTitle == '' || postBody == '') {  // condition to body of the html
+    if (postTitle.trim() == '' || postBody.trim() == '') {  // condition to body of the html
         alert('Please complete the fields')
         return
     }
@@ -57,13 +61,64 @@ function postData(){
         'Content-type': 'application/json; charset=UTF-8',
         },
     })
-    .then(res => res.json())
-    .then(data => {
-        tasks.push(data)
-        renderTaskList()
-        // reset of the fields
-        postTitleInput.value = ''
-        postBodyInput.value = ''
+        .then(res => res.json())
+        .then(data => {
+            tasks.unshift(data)  // first to show
+            renderTaskList()
+            // reset of the fields
+            postTitleInput.value = ''
+            postBodyInput.value = ''
+        })
+        .catch(error => console.error('Error to create a Reminder: ',error))
+}
+
+// function of the button´s
+function editTask(id){    // this is the ID of the buttom created in the function renderTaskList
+    const editForm = document.getElementById(`editForm-${id}`)
+    editForm.style.display = (editForm.style.display == 'none') ? 'block' : 'none'  // action of the button show or hide
+}
+
+function updateTask(id){
+    const editTitle = document.getElementById(`editTitle-${id}`).value
+    const editBody = document.getElementById(`editBody-${id}`).value
+
+    fetch(`${urlBase}/${id}`,{
+        method: 'PUT',
+        body: JSON.stringify({
+            id: id,
+            title: editTitle,
+            body: editBody,
+            userId: 1,
+        }),
+        headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        },
     })
-    .catch(error => console.error('Error to create a Reminder: ',error))
+    .then(res => res.json())
+    .then(data => {  // updating that ID promptly
+        const index = tasks.findIndex(task => task.id === data.id)
+        if(index != -1){
+            tasks[index] = data
+        }else{
+            alert('Error updating task information')
+        }
+        renderTaskList()  // render to make modifications
+    })
+    .catch(error => console.error('Error to update a reminder: ',error))
+}
+
+// action of delete a Task
+function deleteTask(id){
+    fetch(`${urlBase}/${id}`,{
+        method: 'DELETE',
+    })
+    .then(res => {
+        if(res.ok){
+            tasks = tasks.filter(task => task.id != id)   //filter to show task that were not eliminated
+            renderTaskList()
+        }else{
+            alert('Error deleting task')
+        }
+    })
+    .catch(error => console.error('Error deleting a task: ',error))
 }
